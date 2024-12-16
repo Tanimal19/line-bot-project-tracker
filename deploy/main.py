a98db242cdda1439d979c5c0374df873d160fa67
Tanimal <Bobcheng.bob@gmail.com>
2024-12-16 17:10:18 +0800

    modified message content

--- deploy/main.py
+++ deploy/main.py
@@ -149,9 +149,7 @@ def handle_message(event):
                 print("HANDLE_REQUEST > ADD_PROJECT")
 
             user_context.update_state(Status.ADD_PROJECT)
-            send_line_text_message(
-                event, "請說明欲新增的專案內容。\n如果要取消動作，請輸入'cancel'。"
-            )
+            send_line_text_message(event, "請說明欲新增的專案內容。")
             return
 
         # remove project
